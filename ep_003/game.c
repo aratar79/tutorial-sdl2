7fda3138e03ce47cec006e63136fc4f79748aa53
Dani Rodríguez <danirod@outlook.com>
2013-09-10 22:15:00 +0200

    04 - Captura evento SDL_QUIT
    Es necesario procesar los eventos para poder interactuar con la
    aplicación. En este episodio se añade un sistema de lectura de
    eventos y se hace que cuando se reciba el evento SDL_QUIT se cierre
    la ventana de juego.
    
    Vídeo: https://www.youtube.com/watch?v=FeoRoRwXx40

--- ep_003/game.c
+++ ep_003/game.c
@@ -3,6 +3,8 @@
 int main(int argc, char* argv)
 {
     SDL_Window* window = NULL;
+    SDL_Event evento;
+    int juegoEnMarcha = 0;
 
     // 1. INICIAR EL JUEGO
     SDL_Init(SDL_INIT_EVERYTHING);
@@ -14,16 +16,22 @@ int main(int argc, char* argv)
         SDL_Quit();
         return 1;
     }
-
-    // BUCLE [serie de códigos que se repite de forma continua]
-        
+    
+    juegoEnMarcha = 1;
+    while(juegoEnMarcha)
+    {
         // 2. PROCESA LA ENTRADA
+        SDL_PollEvent(&evento);
+        if(evento.type == SDL_QUIT)
+        {
+            juegoEnMarcha = 0;
+            break;
+        }
 
         // 3. ACTUALIZAR EL ESTADO DEL JUEGO
 
         // 4. RENDERIZAR EL JUEGO.
-
-    // CONDICIÓN DE SALIDA DEL BUCLE: el juego termine de funcionar
+    }
 
     // 5. FINALIZA EL JUEGO
     SDL_DestroyWindow(window);
